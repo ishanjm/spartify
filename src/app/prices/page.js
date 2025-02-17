cedcdc7caf84a21d6c28b06989a8d6e8ae070ebb
Ishan Ranasinghe <ishanjm@gmail.com>
2025-02-17 16:29:56 +0530

    added new page

---
+++ src/app/prices/page.js
@@ -0,0 +1,10 @@
+import PageWrapper from "@/components/shared/wrappers/PageWrapper";
+
+const Prices = () => {
+  return (
+    <PageWrapper isInnerPage={true}>
+      <h1>test</h1>
+    </PageWrapper>
+  );
+};
+export default Prices;
